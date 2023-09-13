99fc8000d0d1ed773f8ef9a7a19a118133b36615
monty <monty@plgrim.com>
2023-09-13 14:12:43 +0900

    test

---
+++ main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+resource "aws_iam_user" "monty3" {
+  name = "monty3"
+}
+
+resource "aws_iam_group" "terraform_group" {
+  name = "terraform"
+}
+
+resource "aws_iam_group_policy" "terraform_group_policy" {
+  group = aws_iam_group.terraform_group.name
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : "*",
+          "Resource" : "*"
+        },
+      ]
+    }
+  )
+}
+resource "aws_iam_group_membership" "terraform" {
+  group = aws_iam_group.terraform_group.name
+  name  = "terraform_membership"
+  users = [
+    aws_iam_user.monty3.name
+  ]
+}
+
+#aws_iam_user_group_membership
